d94032697e37520770377217748336991e400e10
Shreyansh Deb <projects.shreyanshdeb@gmail.com>
2020-01-25 14:22:30 +0530

    Reading data from client and writing it back.

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main(){
-	
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Panic(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(60 * time.Second))
+	if err != nil {
+		log.Panic(err)
+	}
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Printf("\nMaine sun liya: %s\n", ln)
+		fmt.Fprintf(conn, "Tune kaha: %s", ln)
+	}
+	conn.Close()
 }
